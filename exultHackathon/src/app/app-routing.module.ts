a52bee0969e55d997a99442b4fac2a03d391bdee
thabet Ahmad <ahmad.j.thabet@gmail.com>
2019-09-21 19:41:52 +0300

    main components

--- exultHackathon/src/app/app-routing.module.ts
+++ exultHackathon/src/app/app-routing.module.ts
@@ -3,10 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import {HospitalComponent} from './Components/hospital/hospital.component';
 import {DonaterComponent} from './Components/donater/donater.component';
 import {LoginComponent} from './Components/login/login.component';
+import { ProfileComponent } from './Components/hospital/profile/profile.component';
+import { RequestDonationComponent } from './Components/hospital/request-donation/request-donation.component';
 
 
 const routes: Routes = [
-  { path: 'hospital', component: HospitalComponent },
+  { path: 'hospital', component: HospitalComponent, children: [
+    {path:'', component: ProfileComponent},
+    {path:'request', component: RequestDonationComponent},
+
+  ]},
   { path: 'donater', component: DonaterComponent },
   { path: 'login', component: LoginComponent }
 ];
