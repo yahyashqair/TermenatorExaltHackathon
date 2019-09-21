3baf58e1c6f63418c5c6ebc4530a2a586ea2d9e8
thabet Ahmad <ahmad.j.thabet@gmail.com>
2019-09-21 18:34:09 +0300

    main components

---
+++ exultHackathon/src/app/Components/donater/donater.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donater',
+  templateUrl: './donater.component.html',
+  styleUrls: ['./donater.component.scss']
+})
+export class DonaterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- exultHackathon/src/app/app.module.ts
+++ exultHackathon/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HospitalComponent } from './Components/hospital/hospital.component';
+import { DonaterComponent } from './Components/donater/donater.component';
+import { LoginComponent } from './Components/login/login.component';
+import { SignupComponent } from './Components/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HospitalComponent,
+    DonaterComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
