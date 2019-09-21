4ac5ddec145c951800f970831616a8bf16372172
thabet Ahmad <ahmad.j.thabet@gmail.com>
2019-09-21 19:22:46 +0300

    main components

--- exultHackathon/src/app/app.module.ts
+++ exultHackathon/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HospitalComponent } from './Components/hospital/hospital.component';
 import { DonaterComponent } from './Components/donater/donater.component';
 import { LoginComponent } from './Components/login/login.component';
 import { SignupComponent } from './Components/signup/signup.component';
+import { ProfileComponent } from './Components/hospital/profile/profile.component';
+import { RequestDonationComponent } from './Components/hospital/request-donation/request-donation.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { SignupComponent } from './Components/signup/signup.component';
     HospitalComponent,
     DonaterComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    ProfileComponent,
+    RequestDonationComponent
   ],
   imports: [
     BrowserModule,
