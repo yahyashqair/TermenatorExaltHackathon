80f21466cbb32ce41dc4b04403d95734d45c00a2
Yahya Shqair <y.shqair@hotmail.com>
2019-09-21 19:08:59 +0300

    add side bar

--- exultHackathon/src/app/app.module.ts
+++ exultHackathon/src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HospitalComponent } from './Components/hospital/hospital.component';
-import { DonaterComponent } from './Components/donater/donater.component';
-import { LoginComponent } from './Components/login/login.component';
-import { SignupComponent } from './Components/signup/signup.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HospitalComponent} from './Components/hospital/hospital.component';
+import {DonaterComponent} from './Components/donater/donater.component';
+import {LoginComponent} from './Components/login/login.component';
+import {SignupComponent} from './Components/signup/signup.component';
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {MenuItem} from 'primeng/api';                 //api
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SidebarModule} from 'primeng/sidebar';
 
 @NgModule({
   declarations: [
@@ -18,9 +22,10 @@ import { SignupComponent } from './Components/signup/signup.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, AccordionModule, BrowserAnimationsModule,SidebarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
