4d850628dc66f7a14430a7be74306f15a3060b72
majd khasib <madj20171998@gmail.com>
2019-09-22 12:44:18 +0300

    the login and sign up is added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {LoginComponent} from './Components/login/login.component';
 import {ProfileComponent} from './Components/hospital/profile/profile.component';
 import {RequestDonationComponent} from './Components/hospital/request-donation/request-donation.component';
 import {DonationRequestsComponent} from './Components/donation-requests/donation-requests.component';
+import { SignupComponent } from './Components/signup/signup.component';
 
 
 const routes: Routes = [
@@ -17,6 +18,7 @@ const routes: Routes = [
   },
   {path: 'donater', component: DonaterComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'signUp', component: SignupComponent},
   {path: 'requests', component: DonationRequestsComponent}
 
 ];
