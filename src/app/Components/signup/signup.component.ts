054fba101aabe92d476028e957ed979cb8dab002
majd khasib <madj20171998@gmail.com>
2019-09-22 16:07:27 +0300

    hi

--- src/app/Components/signup/signup.component.ts
+++ src/app/Components/signup/signup.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {AuthService} from '../../Services/auth.service';
-import {HospitalService} from '../../Services/hospital.service';
-import {DonaterModel} from '../../models/donater.model';
-import {DonaterService} from '../../Services/donater-service.service';
-import {HospitalModel} from '../../models/hospital.model';
+import { Component, OnInit, ViewChild, OnChanges } from '@angular/core';
+import { AuthService } from '../../Services/auth.service';
+import { HospitalService } from '../../Services/hospital.service';
+import { DonaterModel } from '../../models/donater.model';
+import { DonaterService } from '../../Services/donater-service.service';
+import { HospitalModel } from '../../models/hospital.model';
 
 @Component({
   selector: 'app-signup',
@@ -11,34 +11,52 @@ import {HospitalModel} from '../../models/hospital.model';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-
-  constructor(public  authService: AuthService, private hospitalService: HospitalService, private donaterService: DonaterService) {
+  nameType:string='donater';
+  constructor(public authService: AuthService, private hospitalService: HospitalService, private donaterService: DonaterService) {
   }
 
   ngOnInit() {
-  }
 
-  sign(name: string, birthday: string, gender: string, phone: string, email: string, password: string, type: string) {
-    console.log(type);
+  }
+  isDonator() {
+    console.log(this.nameType);
+    console.log(this.nameType == 'donater');
+    return this.nameType === 'donater';
+  }
+  isHostpital() {
+    console.log(this.nameType);
+    console.log(this.nameType == 'hospital');
+    return this.nameType === 'hospital';
+  }
+  changeVar(type:string)
+  {
+this.nameType=type;
+  }
+  signDonator(name: string, birthday: string, gender: string,bloodType:string, phone: string,Address:string, email: string, password: string) {
+ 
     this.authService.signUp(email, password);
-    if (type === 'hospital') {
-      console.log('idiot');
-      let hospital : HospitalModel = new HospitalModel();
-      hospital.phoneNumber=phone;
-      hospital.name=name;
-      hospital.email=email;
-      hospital.city='ramallah';
-      this.hospitalService.saveHospital(hospital);
-    }if (type === 'donater') {
-      let donator : DonaterModel = new DonaterModel();
-      donator.phone=phone;
-      donator.city='ramallah';
-      donator.name=name;
-      donator.gender=gender;
-      donator.bloodType='AB';
+ 
+      let donator: DonaterModel = new DonaterModel();
+      donator.phone = phone;
+      donator.city = Address;
+      donator.name = name;
+      donator.gender = gender;
+      donator.bloodType = bloodType;
+      console.log(donator);
       this.donaterService.saveDonater(donator);
-      console.log('notidiot');
-    }
+    
 
   }
-}
+  signHospital(name:string,Address:string,phone:string,email:string, password:string){
+  
+    this.authService.signUp(email, password);
+      let hospital: HospitalModel = new HospitalModel();
+      hospital.phoneNumber = phone;
+      hospital.name = name;
+      hospital.email = email;
+      hospital.city = Address;
+      console.log("55555");
+      this.hospitalService.saveHospital(hospital);
+    } 
+  }
+
