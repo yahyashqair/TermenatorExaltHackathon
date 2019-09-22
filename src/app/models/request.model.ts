98228774cdf8cc2a941baf410ae9ac489d2dc2ad
thabet Ahmad <ahmad.j.thabet@gmail.com>
2019-09-22 13:02:05 +0300

    Update request.model.ts

--- src/app/models/request.model.ts
+++ src/app/models/request.model.ts
@@ -1,17 +1,8 @@
 import {HospitalModel} from './hospital.model';
 
 export class RequestModel {
-  constructor(hospital: HospitalModel, msg: string, bloodType: string, value: string) {
-    this.hospital = hospital;
-    this.msg = msg;
-    this.bloodType = bloodType;
-    this.value = value;
-  }
-
-  hospital: HospitalModel;
   id: number;
-  msg: string;
+  hospital: HospitalModel;
   bloodType: string;
-  value: string;
-
+  quantity: string;
 }
